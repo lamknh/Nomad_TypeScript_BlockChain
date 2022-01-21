fa56b89aad69f80f9fa07df8d3900c2fc6c8c2c2
Kim Nahyeong <of.kimna@gmail.com>
2022-01-21 14:48:26 +0900

    완강

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,114 @@
-class Block {
-    public index: number;
-    public hash: string;
-    public previousHash: string;
-    public data: string;
-    public timestamp: number;
+import * as CryptoJS from "crypto-js";
 
-    constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
-        this.index = index;
-        this.hash = hash;
-        this.previousHash = previousHash;
-        this.data = data;
-        this.timestamp = timestamp;
-    }
+class Block {
+  //static method : 클래스 생성되지 않아도 호출 가능 - 항상 사용 가능
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  // 구조 맞으면 True 아니면 False
+  static validateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
 const genesisBlock: Block = new Block(0, "2202020202020", "", "Hello", 123456);
 
-let blockchian: [Block] = [genesisBlock]; // Block만 블록체인에 추가하도록 체크
+let blockchain: Block[] = [genesisBlock]; // Block만 블록체인에 추가하도록 체크
 
-console.log(blockchian);
+const getBlockChain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimestamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+
+  // 블록 생성할 때 마다 블록체인에 추가하기
+  addBlock(newBlock);
+
+  return newBlock;
+};
+
+const getHashforBlock = (aBlock: Block): string =>
+  Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    // 구조 검사
+    return false;
+  } else if (previousBlock.index + 1 != candidateBlock.index) {
+    // 인덱스 검사
+    return false;
+  } else if (previousBlock.hash != candidateBlock.previousHash) {
+    // 이전 블록 해시 검사
+    return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    // 해시 검사
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("forth block");
+
+console.log(blockchain);
 
 export {};
